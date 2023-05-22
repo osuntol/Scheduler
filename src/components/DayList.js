cb585d9e2631be09f4ccb92d5bf2aec63f777af3
Rotimi Osuntola Jr <rosuntola@gmail.com>
2023-05-22 20:31:36 +0000

    Day List refractored to return day list items

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,10 +1,19 @@
 import React from "react";
 import DayListItem from "./DayListItem";
 
-export default function DayList(props){
+export default function DayList(props) {
+
   const dayItems = props.days.map((day) => {
-   <DayListItem key={day.id} name={day.name} spots={day.spots}/>
-  })
+    return (
+    <DayListItem 
+    key={day.id} 
+    name={day.name} 
+    spots={day.spots}
+    selected={day.name === props.day}
+    setDay ={props.setDay} 
+    />
+  )
+    })
   return (
     <ul>
       {dayItems}
