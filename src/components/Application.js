68cc1f4eef59fb30ec75cfb4ebb04dbdabf5ce9b
Rotimi Osuntola Jr <rosuntola@gmail.com>
2023-05-29 20:39:20 -0400

    Application file updated with axios get req

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,29 +1,66 @@
 import React from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
-import React, {useState} from "react";
+import { useState } from "react";
+import Appointment from "./Appointment";
+import { useEffect } from "react";
+import Axios from "axios";  
 
-
-const days = [
-  {
+const appointments = {
+  "1": {
     id: 1,
-    name: "Monday",
-    spots: 2,
+    time: "12pm",
   },
-  {
+  "2": {
     id: 2,
-    name: "Tuesday",
-    spots: 5,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
   },
-  {
+  "3": {
     id: 3,
-    name: "Wednesday",
-    spots: 0,
+    time: "2pm",
   },
-];
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
+
+
 
 export default function Application(props) {
-  const [value, onChange] = useState('Monday')
+  const [days, setDays] = useState([]);
+  const [value, onChange] = useState('Monday');
+
+  const URL = 'http://localhost:8001/api/days'
+  useEffect(()=>{
+      Axios
+      .get(URL)
+      .then((response) => {
+       setDays(response.data)
+      })
+  }, [])
+
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -48,7 +85,10 @@ export default function Application(props) {
         {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {Object.values(appointments).map((appointment) => (<Appointment
+          key={appointment.id}
+          {...appointment}
+        />))}
       </section>
     </main>
   );
