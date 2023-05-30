6629ddd3d01603ce86362732f09aeb9944d9aaba
Rotimi Osuntola Jr <rosuntola@gmail.com>
2023-05-29 20:42:48 -0400

    Daylistitem file updated

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,5 +1,5 @@
 import React from "react";
-import DayListItemscss from 'components/DayListItem.scss'
+import 'components/DayListItem.scss'
 import classNames from "classnames";
 
 export default function DayListItem(props) {
