90acb8475bcf7f5b47c724d05ca4650f2341c6f5
Rotimi Osuntola Jr <rosuntola@gmail.com>
2023-06-05 22:32:15 -0400

    Changed a value within the file

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -6,15 +6,15 @@ export default function DayList(props) {
   const dayItems = props.days.map((day) => {
 
     return (
-    <DayListItem 
-    key={day.id} 
-    name={day.name} 
-    spots={day.spots}
-    selected={day.name === props.value}
-    setDay ={props.onChange} 
-    />
-  )
-    })
+      <DayListItem
+        key={day.id}
+        name={day.name}
+        spots={day.spots}
+        selected={day.name === props.day}
+        setDay={props.setDay}
+      />
+    )
+  })
   return (
     <ul>
       {dayItems}
