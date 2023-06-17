0c6ea8168fd9934bc46c31328d1dcd7a690e95e2
Rotimi Osuntola Jr <rosuntola@gmail.com>
2023-06-17 01:58:49 -0400

    Multiple functions added to refaractor Form js file

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,23 +5,37 @@ import InterviewerList from "components/InterviewerList";
 
 
 export default function Form(props) {
-  
+
   //using state hooks to set values 
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
-  
+  const [error, setError] = useState("");
+
+
   //reset function the resets the values
-  function reset(){
+  function reset() {
     setStudent("")
     setInterviewer(null)
   };
 
   //function to pass into cancel onClick to reset the value
-  function cancel(){
+  function cancel() {
     reset();
     props.onCancel();
   };
 
+  //function to check if student name is blank
+  function validate() {
+    if (student === "") {
+      setError("student name cannot be blank");
+      return;
+    }
+    if (interviewer === null) {
+      setError("Please select an interviewer");
+      return;
+    }
+    props.onSave(student, interviewer);
+  }
 
   return (
     <main className="appointment__card appointment__card--create">
@@ -32,20 +46,22 @@ export default function Form(props) {
             name="name"
             type="text"
             placeholder="Enter Student Name"
+            value={student}
             onChange={(event) => setStudent(event.target.value)}
+            data-testid="student-name-input"
           />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList
           interviewers={props.interviewers}
-          student={student}
-          value= {interviewer}
+          value={interviewer}
           onChange={setInterviewer}
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={()=>{props.onSave(student, interviewer)}}>Save</Button>
+          <Button confirm onClick={validate}>Save</Button>
         </section>
       </section>
     </main>
