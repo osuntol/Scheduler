14d60366c64e49e7a23a5a73d696c1512502caa4
Rotimi Osuntola Jr <rosuntola@gmail.com>
2023-06-19 23:30:27 -0400

    updated file to allow tests to pass

--- src/components/__tests__/Appointment.test.js
+++ src/components/__tests__/Appointment.test.js
@@ -4,16 +4,8 @@ import Appointment from "components/Appointment";
 
 afterEach(cleanup);
 
-// describe("Appointment", () => {
-//   it("renders without crashing", () => {
-//     render(<Appointment />);
-//   });
-
-  // it("does something it is supposed to do", () => {
-  //   // ...
-  // });
-
-  // it("does something else it is supposed to do", () => {
-  //   // ...
-  // });
-// });
+describe("Appointment", () => {
+  it("renders without crashing", () => {
+    render(<Appointment />);
+  });
+})
