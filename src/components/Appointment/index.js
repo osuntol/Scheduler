09db67307e44ac416f0a70cab0d74bff55cc2534
Rotimi Osuntola Jr <rosuntola@gmail.com>
2023-06-19 23:25:52 -0400

    index js file updated

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -50,7 +50,8 @@ export default function Appointment(props) {
   }
 
   return (
-    <article className="appointment">
+    <article className="appointment"
+    data-testid="appointment">
 
       <Header
         time={props.time}
